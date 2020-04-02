a92fd1585f3afc3ef06ed44e03d070ae24500aa7
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2020-04-02 13:52:05 +0200

    Fix stuff

--- mail-handler/src/main/java/de/ihrigb/fwla/mail/EmailBodyConverter.java
+++ mail-handler/src/main/java/de/ihrigb/fwla/mail/EmailBodyConverter.java
@@ -9,7 +9,7 @@ public interface EmailBodyConverter<T> {
 	 * Convert the received message to the desired format. It may throw
 	 * {@see EmailBodyConvertionException} upon failure.
 	 *
-	 * @param mimeMessage
+	 * @param mimeMessage the mime message to be converted
 	 * @return the converted value
 	 */
 	T convert(MimeMessage mimeMessage);
